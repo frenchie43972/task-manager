4ad69a06cb5f0e4fd9ebe2689359fb285b68fda1
Kris French <frenchie43972@gmail.com>
2026-02-03 13:34:50 +0900

    refactoring

--- frontend/task-manager/src/stores/taskStore.js
+++ frontend/task-manager/src/stores/taskStore.js
@@ -4,20 +4,18 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 export const useTaskStore = defineStore('tasks', {
   state: () => ({
     tasks: [],
-    loading: false,
     error: null,
   }),
 
   actions: {
     async fetchTasks() {
-      this.loading = true
       this.error = null
 
       try {
         const res = await fetch(`${API_BASE_URL}/tasks`)
 
         if (!res.ok) {
-          throw new Error('Failed to fetch notes.')
+          throw new Error('Failed to fetch tasks.')
         }
 
         // Reads the JSON response from the body
@@ -25,27 +23,26 @@ export const useTaskStore = defineStore('tasks', {
 
         // Updates the reactive state triggering a rerender
         this.tasks = data
+        this.error = null
       } catch (err) {
         this.error = err.message
-      } finally {
-        this.loading = false
       }
     },
 
-    async createTask(title, priority, details) {
+    async createTask(taskData) {
       const res = await fetch(`${API_BASE_URL}/tasks`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, priority, details }),
+        body: JSON.stringify(taskData),
       })
 
       if (!res.ok) {
         throw new Error('Failed to create task')
       }
 
-      const newTask = await res.json()
+      const createdTask = await res.json()
 
-      this.tasks.push(newTask)
+      this.tasks.push(createdTask)
     },
 
     async deleteTask(id) {
@@ -60,11 +57,11 @@ export const useTaskStore = defineStore('tasks', {
       this.tasks = this.tasks.filter((task) => task.id !== id)
     },
 
-    async updateTask(id, title, priority, details) {
+    async updateTask(id, updates) {
       const res = await fetch(`${API_BASE_URL}/tasks/${id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, priority, details }),
+        body: JSON.stringify(updates),
       })
 
       if (!res.ok) {
