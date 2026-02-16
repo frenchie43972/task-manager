ed00a9863a7277664bb64e54d6f83b2f1f8afd2a
Kris French <frenchie43972@gmail.com>
2026-02-16 17:05:45 +0900

    Still working FE refactor

--- frontend/task-manager/src/components/TaskItem.vue
+++ frontend/task-manager/src/components/TaskItem.vue
@@ -1,67 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { useTaskStore } from '@/stores/taskStore.js'
 
-const props = defineProps({
+const { task } = defineProps({
   task: {
     type: Object,
     required: true,
   },
 })
 
+const router = useRouter()
 const taskStore = useTaskStore()
 
-const isEditing = ref(false)
-const editTitle = ref('')
-const editPriority = ref('')
-const editDetails = ref('')
-
-function startEdit() {
-  isEditing.value = true
-  editTitle.value = props.task.title
-  editPriority.value = props.task.priority
-  editDetails.value = props.task.details ?? ''
-}
-
-async function saveEdit() {
-  await taskStore.updateTask(props.task.id, {
-    title: editTitle.value,
-    priority: editPriority.value,
-    details: editDetails.value,
+function goToEdit() {
+  router.push({
+    name: 'task-edit',
+    params: { id: task.id },
   })
-
-  isEditing.value = false
-}
-
-function cancelEdit() {
-  isEditing.value = false
 }
 </script>
 
 <template>
   <li>
-    <div v-if="isEditing">
-      <input v-model="editTitle" />
-
-      <select v-model="editPriority">
-        <option value="High">High</option>
-        <option value="Medium">Medium</option>
-        <option value="Low">Low</option>
-      </select>
-
-      <textarea v-model="editDetails"></textarea>
-
-      <button @click="saveEdit">Save</button>
-      <button @click="cancelEdit">Cancel</button>
-    </div>
-
-    <div v-else>
+    <div>
       <h3>{{ task.title }}</h3>
       <p>{{ task.priority }}</p>
       <p>{{ task.details }}</p>
 
-      <button @click="taskStore.deleteTask(props.task.id)">Delete</button>
-      <button @click="startEdit">Edit</button>
+      <button @click="taskStore.deleteTask(task.id)">Delete</button>
+      <button @click="goToEdit">Edit</button>
     </div>
   </li>
 </template>
--- frontend/task-manager/src/views/TaskFormView.vue
+++ frontend/task-manager/src/views/TaskFormView.vue
@@ -1,7 +1,36 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useTaskStore } from '@/stores/taskStore'
+import TaskForm from '@/components/TaskForm.vue'
+
+const route = useRoute()
+const router = useRouter()
+const taskStore = useTaskStore()
+
+// If route has an id param, it can be edited
+const isEditMode = computed(() => !!route.params.id)
+
+// Finds the task in the store if editing
+const task = computed(() => {
+  if (!isEditMode.value) return null
+
+  return taskStore.tasks.find((t) => t.id === Number(route.params.id))
+})
+
+// After saving, return to the home page
+function handleSuccess() {
+  router.push({ name: 'tasks' })
+}
+</script>
 
 <template>
-  <h2>Hi There!</h2>
+  <div>
+    <h1 v-if="isEditMode">Edit Task</h1>
+    <h1 v-else>Create Task</h1>
+
+    <TaskForm :task="task" :isEdit="isEditMode" @success="handleSuccess" />
+  </div>
 </template>
 
 <style scoped></style>
